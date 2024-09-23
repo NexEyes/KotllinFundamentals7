c54c4c6f9c97dfee7d46a3bb9b9d3aeecfeabe36
Keith Shovan <shovakei000@warren.k12.in.us>
2024-09-23 11:48:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kotllinfundamentals7/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.kotllinfundamentals7
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+open class Phone(var isScreenOn: Boolean = false){
+    open fun switchOn() {
+        isScreenOn = true
+    }
+
+    fun switchOff() {
+        isScreenOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenOn) "on" else "off"
+        println("The phone's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(var isFolded: Boolean = true): Phone() {
+    override fun switchOn() {
+        if (!isFolded) {
+            isScreenOn = true
+        }
+    }
+
+    fun folded() {
+        isFolded = true
+    }
+
+    fun unfold() {
+        isFolded = false
+    }
+}
+
+fun main() {
+    val newFoldablePhone = FoldablePhone()
+
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.unfold()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+}
